803fa64a3f744a986a539b9032774e3e4c7be92b
Lucas <lucaslugui1604@gmail.com>
2025-04-14 09:31:28 -0300

    respondendo terceira pergunta

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,7 @@ console.log(typeof vazia);
 let valor = 1
 console.log(typeof valor);
 
-// Como usar o operador os operadores( != , ==, >= ) em JavaScript? Exemplifique e demonstre a saída!
+//2 - Como usar o operador os operadores( != , ==, >= ) em JavaScript? Exemplifique e demonstre a saída!
 
 const a = 10
 const b = 20
@@ -18,6 +18,25 @@ console.log(a >= b);
 console.log(a <= b);
 console.log(a == b);
 console.log(a != b);
+
+// 3 -  Escreva um programa(usando Switch case) que calcula o índice de massa corporal (IMC) de uma pessoa e exibe uma mensagem indicando se ela está abaixo, no peso ideal ou acima do peso. Exemplifique e demonstre a saída!
+
+const peso = prompt('qual o seu peso? (em kg)' )
+const altura = prompt('qual a sua altura? (em M)')
+const imc = peso / (altura * altura)
+console.log(imc);
+
+switch (imc){
+    case imc >= 24,9:
+        alert(`voce esta acima do peso`);
+        break
+    case imc <= 18,5:
+        alert(`voce esta abaixo do peso`)
+        break
+    default:
+        alert(`voce esta no peso ideal`)
+}
+
 
 
 
