5981b1574200de5745374ae435bd2c48fbe9a549
Lucas <lucaslugui1604@gmail.com>
2025-04-14 09:20:55 -0300

    respondendo primeira pergunta e adicionando js ao html

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+//1  - Como declarar uma variável com valor undefined, vazio e com valor em JavaScript? Exemplifique e demonstre a saída do valor da variável!
+
+let indefinida;
+console.log(typeof indefinida);
+
+let vazia = []
+console.log(typeof vazia);
+
+let valor = 1
+console.log(typeof valor);
+
+
+
+
+
+
